465168745a6b62e46676e08b80bf1613d362d13b
pnonell <pau.nonell@gmail.com>
2019-01-02 18:35:21 +0100

    videos

--- tizen/scripts/utils/constant.js
+++ tizen/scripts/utils/constant.js
@@ -35,7 +35,61 @@ var CONSTANT = {
     	          color: 'rgba(255, 0, 0, .3)',
     	          price: 3.99,
     	          business_price: 14.50
-    	      }       
+    	      },
+    	      {
+    	    	  description: 'China has a saying; to see the past, visit Beijing, to see the present, go to Shanghai but for the future, it’s Shenzhen.',
+    	    	  id: 'tech_1',
+    	    	  name: 'Shenzhen: The city of the future',
+    	    	  author: 'RT Documentary',
+    	    	  date: '08/07/2017',
+    	    	  duration: '25:54',
+    	    	  source: 'https://www.dropbox.com/s/8tnqxv51e0zadnl/Shenzhen-Tech.mp4?dl=1',
+    	    	  photo_urls: [
+    	                       {
+    	                           size: '240x180',
+    	                           url: 'https://www.dropbox.com/s/bs0g7we0oh83izc/shenzhen.png?dl=1'
+    	                       }
+    	                   ],
+    	          color: 'rgba(255, 0, 0, .3)',
+    	          price: 2.99,
+    	          business_price: 14.50
+    	      },
+    	      {
+    	    	  description: 'People in green screen suits, monsters that arent actually there and much more. Come behind the scenes and take a look what really went on in the making of "Fantastic Beasts and Where to Find Them".',
+    	    	  id: 'tech_2',
+    	    	  name: 'Special Effects on Fantastic Beasts',
+    	    	  author: 'Fame Focus',
+    	    	  date: '19/12/2018',
+    	    	  duration: '9:00',
+    	    	  source: 'https://www.dropbox.com/s/vtl4zxxm0gw667c/SpecialEffects-Tech.mp4?dl=1',
+    	    	  photo_urls: [
+    	                       {
+    	                           size: '240x180',
+    	                           url: 'https://www.dropbox.com/s/sosbye0jtq7rm3f/specialeffects.png?dl=1'
+    	                       }
+    	                   ],
+    	          color: 'rgba(255, 0, 0, .3)',
+    	          price: 1.99,
+    	          business_price: 9.50
+    	      },
+    	      {
+    	    	  description: 'Artificial Intelligence: Mankinds Last Invention - Technological Singularity Explained',
+    	    	  id: 'tech_3',
+    	    	  name: 'Artificial Intelligence',
+    	    	  author: 'Aperture',
+    	    	  date: '05/10/2018',
+    	    	  duration: '20:21',
+    	    	  source: 'https://www.dropbox.com/s/8y4rx6y84gtil9n/AI-Tech.mp4?dl=1',
+    	    	  photo_urls: [
+    	                       {
+    	                           size: '240x180',
+    	                           url: 'https://www.dropbox.com/s/asb1ldo6mq7k7ew/ai.png?dl=1'
+    	                       }
+    	                   ],
+    	          color: 'rgba(255, 0, 0, .3)',
+    	          price: 3.50,
+    	          business_price: 18.50
+    	      }
     	 ],
     	 BIOLOGY: [
     	    	      {
@@ -55,7 +109,61 @@ var CONSTANT = {
     	    	          color: 'rgba(255, 0, 0, .3)',
     	    	          price: 4.50,
     	    	    	  business_price: 20.99
-    	    	      }       
+    	    	      },
+    	    	      {
+    	    	    	  description: 'An exploration of the structure of deoxyribonucleic acid, or DNA. ',
+    	    	    	  id: 'bio_1',
+    	    	    	  name: 'DNA',
+    	    	    	  author: 'MITx Bio',
+    	    	    	  date: '26/05/2015',
+    	    	    	  duration: '5:58',	 
+    	    	    	  source: 'https://www.dropbox.com/s/p5hxahi4sehaljs/DNA-Bio.mp4?dl=1',
+    	    	    	  photo_urls: [
+    	    	                       {
+    	    	                           size: '240x180',
+    	    	                           url: 'https://www.dropbox.com/s/mob1ebr68vx53jf/dna.png?dl=1'
+    	    	                       }
+    	    	                   ],
+    	    	          color: 'rgba(255, 0, 0, .3)',
+    	    	          price: 1.50,
+    	    	    	  business_price: 5.99
+    	    	      },
+    	    	      {
+    	    	    	  description: 'An animation/video teaching the basics of how cancer forms and spreads.  Topics include: mutation, tumor suppressors, oncogenes, angiogenesis, apoptosis, metastasis and drug resistance.  ',
+    	    	    	  id: 'bio_2',
+    	    	    	  name: 'Cancer',
+    	    	    	  author: 'CancerQuest',
+    	    	    	  date: '02/10/2013',
+    	    	    	  duration: '12:07',	 
+    	    	    	  source: 'https://www.dropbox.com/s/okrx0xjo9n7zxwt/Cancer-Bio.mp4?dl=1',
+    	    	    	  photo_urls: [
+    	    	                       {
+    	    	                           size: '240x180',
+    	    	                           url: 'https://www.dropbox.com/s/0c45f7wzcf5vvum/cancer.png?dl=1'
+    	    	                       }
+    	    	                   ],
+    	    	          color: 'rgba(255, 0, 0, .3)',
+    	    	          price: 2.50,
+    	    	    	  business_price: 7.99
+    	    	      },
+    	    	      {
+    	    	    	  description: 'In which Hank does some push ups for science and describes the "economy" of cellular respiration and the various processes whereby our bodies create energy in the form of ATP.',
+    	    	    	  id: 'bio_3',
+    	    	    	  name: 'ATP & Respiration',
+    	    	    	  author: 'CrashCourse',
+    	    	    	  date: '12/03/2012',
+    	    	    	  duration: '13:25',	 
+    	    	    	  source: 'https://www.dropbox.com/s/xdphjb1gpypo4jb/ATP-Bio.mp4?dl=1',
+    	    	    	  photo_urls: [
+    	    	                       {
+    	    	                           size: '240x180',
+    	    	                           url: 'https://www.dropbox.com/s/yqdi3bvffsbgsfl/atp.png?dl=1'
+    	    	                       }
+    	    	                   ],
+    	    	          color: 'rgba(255, 0, 0, .3)',
+    	    	          price: 3.50,
+    	    	    	  business_price: 16.99
+    	    	      }   
     	 ],
     	 SOCIOLOGY: [
  	    	      {
@@ -75,27 +183,135 @@ var CONSTANT = {
  	    	          color: 'rgba(255, 0, 0, .3)',
  	    	          price: 3.50,
  	    	    	  business_price: 18.99
- 	    	      }       
+ 	    	      },
+ 	    	      {
+ 	    	    	  description: 'The independence vote in the north-eastern region of Catalonia shook Spain’s democracy to the core. The Spanish authorities used force to try and stop it, but more than two million Catalans defied the police to back a new independent republic. Nine months on, Catalonia is still part of Spain, its leaders are in prison or abroad and its people are deeply split on the region’s future.',
+ 	    	    	  id: 'soci_1',
+ 	    	    	  name: 'Catalonia independence',
+ 	    	    	  author: 'BBC',
+ 	    	    	  date: '24/07/2018',
+ 	    	    	  duration: '23:03',	 
+ 	    	    	  source: 'https://www.dropbox.com/s/12ayepgt0x4alj7/Catalonia-Soci.mp4?dl=1',
+ 	    	    	  photo_urls: [
+ 	    	                       {
+ 	    	                           size: '240x180',
+ 	    	                           url: 'https://www.dropbox.com/s/oksm6phwqjz6iow/catalonia.png?dl=1'
+ 	    	                       }
+ 	    	                   ],
+ 	    	          color: 'rgba(255, 0, 0, .3)',
+ 	    	          price: 2.50,
+ 	    	    	  business_price: 16.99
+ 	    	      },
+ 	    	     {
+ 	    	    	  description: 'In which John Green teaches you about American women in the Progressive Era and, well, the progress they made. So the big deal is, of course, the right to vote women gained when the 19th amendment was passed and ratified. But women made a lot of other gains in the 30 years between 1890 and 1920.',
+ 	    	    	  id: 'soci_2',
+ 	    	    	  name: 'Womens Suffrage',
+ 	    	    	  author: 'CrashCourse',
+ 	    	    	  date: '26/11/2013',
+ 	    	    	  duration: '13:30',	 
+ 	    	    	  source: 'https://www.dropbox.com/s/fs22vdb0jlfgi85/WomenSuffrage-Soci.mp4?dl=1',
+ 	    	    	  photo_urls: [
+ 	    	                       {
+ 	    	                           size: '240x180',
+ 	    	                           url: 'https://www.dropbox.com/s/zl4c2xz17i1l44k/womensuffrage.png?dl=1'
+ 	    	                       }
+ 	    	                   ],
+ 	    	          color: 'rgba(255, 0, 0, .3)',
+ 	    	          price: 1.50,
+ 	    	    	  business_price: 12.99
+ 	    	      },
+ 	    	     {
+ 	    	    	  description: '"Are you going to win?" the journalist asked the rebel."We don t deserve to lose," the rebel answered. Twenty years after the Zapatista uprising, VICE traveled to Chiapas to meet Morquecho, the first local journalist to speak with the Zapatista Army face-to-face, so he could recall the events of that fateful day—it was the first indigenous armed uprising in Latin America in the internet age.',
+ 	    	    	  id: 'soci_3',
+ 	    	    	  name: 'The Zapatista Uprising',
+ 	    	    	  author: 'Vice',
+ 	    	    	  date: '14/01/2014',
+ 	    	    	  duration: '12:39',	 
+ 	    	    	  source: 'https://www.dropbox.com/s/rfqgkgdi192lbn2/Zapatistas-Soci.mp4?dl=1',
+ 	    	    	  photo_urls: [
+ 	    	                       {
+ 	    	                           size: '240x180',
+ 	    	                           url: 'https://www.dropbox.com/s/7ui3h0fbd2huluv/zapatistas.png?dl=1'
+ 	    	                       }
+ 	    	                   ],
+ 	    	          color: 'rgba(255, 0, 0, .3)',
+ 	    	          price: 3.80,
+ 	    	    	  business_price: 18.99
+ 	    	      },
  	      ],
-    	 POLITICS: [
+    	 GRAPHIC_DESIGN: [
     	      {
-    	    	  description: 'This video shows the borders and populations of each country in Europe, for every year since 400 BC.',
-    	    	  id: 'poli_0',
-    	    	  name: 'The History of Europe',
-    	    	  author: 'Cottereau',
-    	    	  date: '01/01/2018',
-    	    	  duration: '11:44',	 
-    	    	  source: 'https://www.dropbox.com/s/0gxhdts9rbwy5ms/Europe-Poli.mp4?dl=1',
+    	    	  description: 'In this video, you’ll learn the fundamentals of graphic design.',
+    	    	  id: 'graph_0',
+    	    	  name: 'Fundamentals of Graphic Design',
+    	    	  author: 'GCFLearnFree',
+    	    	  date: '13/07/2017',
+    	    	  duration: '6:25',	 
+    	    	  source: 'https://www.dropbox.com/s/6avhn806yr4uouj/Fundamentals-graph.mp4?dl=1',
     	    	  photo_urls: [
     	                       {
     	                           size: '240x180',
-    	                           url: 'https://www.dropbox.com/s/5zqctkqdv7numoa/europe.png?dl=1'
+    	                           url: 'https://www.dropbox.com/s/ejkzqoiagmxy19c/fundamentals.png?dl=1'
     	                       }
     	                   ],
     	          color: 'rgba(255, 0, 0, .3)',
     	          price: 2.99,
     	    	  business_price: 10.99
-    	      }       
+    	      },
+    	      {
+    	    	  description: 'This is effect is for beginners and people who know photoshop both. So without any worry just watch it.',
+    	    	  id: 'graph_1',
+    	    	  name: 'Photoshop: Photo manipulation',
+    	    	  author: 'Photoshop Tutorials',
+    	    	  date: '12/07/2016',
+    	    	  duration: '10:49',	 
+    	    	  source: 'https://www.dropbox.com/s/eprkgxd8phraqvp/PhotoManipulation-Graph.mp4?dl=1',
+    	    	  photo_urls: [
+    	                       {
+    	                           size: '240x180',
+    	                           url: 'https://www.dropbox.com/s/r8cawv8j20u1uqi/photomanipulation.png?dl=1'
+    	                       }
+    	                   ],
+    	          color: 'rgba(255, 0, 0, .3)',
+    	          price: 0.99,
+    	    	  business_price: 5.50
+    	      },
+    	      {
+    	    	  description: 'As human beings, we get used to "the way things are" really fast. But for designers, the way things are is an opportunity ... Could things be better? How? In this funny, breezy talk, the man behind the iPod and the Nest thermostat shares some of his tips for noticing — and driving — change.',
+    	    	  id: 'graph_2',
+    	    	  name: 'Tony Fadell: Secret of Design',
+    	    	  author: 'TED',
+    	    	  date: '03/06/2015',
+    	    	  duration: '16:41',	 
+    	    	  source: 'https://www.dropbox.com/s/c9xu6pxe0m2yr2z/SecretDesign-Graph.mp4?dl=1',
+    	    	  photo_urls: [
+    	                       {
+    	                           size: '240x180',
+    	                           url: 'https://www.dropbox.com/s/swfuo47x7mdody7/secretDesign.png?dl=1'
+    	                       }
+    	                   ],
+    	          color: 'rgba(255, 0, 0, .3)',
+    	          price: 2.99,
+    	    	  business_price: 16.50
+    	      },
+    	      {
+    	    	  description: 'How to design a quality poster, using my poster designing tips. The tips that I have for you on how to design a poster, all are very relevant and will enable you to design a quality poster. So tune in and watch this tutorial on poster design top tips!',
+    	    	  id: 'graph_3',
+    	    	  name: 'Poster designing tips',
+    	    	  author: 'Satori Graphics',
+    	    	  date: '19/01/2018',
+    	    	  duration: '8:25',	 
+    	    	  source: 'https://www.dropbox.com/s/d1x4o0hiy8rlkib/Posters-Graph.mp4?dl=1',
+    	    	  photo_urls: [
+    	                       {
+    	                           size: '240x180',
+    	                           url: 'https://www.dropbox.com/s/q27048au5b1njfi/posters.png?dl=1'
+    	                       }
+    	                   ],
+    	          color: 'rgba(255, 0, 0, .3)',
+    	          price: 4.99,
+    	    	  business_price: 21.50
+    	      }        
     	 ]
     },
     PREPARED_DATA: {
